cf1fedeafe4ae0788672a860ff4440a532eb38ae
iStre1ok <am-nf@yandex.ru>
2022-11-19 13:49:00 +0300

    start project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function getTimeFromMinutes(num) {
+    let h, m;
+  
+    if () {
+  
+    } else {
+      console.log('Ошибка, проверьте данные');
+    }
+  
+    console.log(`Это ${h} часа и ${m} минут`);
+  }
+  
+  getTimeFromMinutes(150);
+  
+  // getTimeFromMinutes(150) => "Это 2 часа и 30 минут"
+  // getTimeFromMinutes(50) => "Это 0 часов и 50 минут"
+  // getTimeFromMinutes(0) => "Это 0 часов и 0 минут"
+  // getTimeFromMinutes(-150) => "Ошибка, проверьте данные"
